5f5ca72471e258abcaf6de9049d9db0178def221
kashyaba <iamkashyab@gmail.com>
2024-06-07 02:44:33 -0700

    capture all the connected and trusted bluetooth devices

--- poll.py
+++ poll.py
@@ -6,6 +6,7 @@ from typing import List
 from enum import StrEnum
 
 class BluezAdapter1Keys(StrEnum):
+    interface = "org.bluez.Adapter1"
     device_info = "org."
     address = "Address"
     address_type = "AddressType"
@@ -23,6 +24,7 @@ class BluezAdapter1Keys(StrEnum):
     uuids = "UUIDs"
     
 class BluezDevice1Keys(StrEnum):
+    interface = "org.bluez.Device1"
     adapter = "Adapter"
     address = "Address"
     address_type = "AddressType"
@@ -65,10 +67,24 @@ manager = QDBusInterface('org.bluez', "/", "org.freedesktop.DBus.ObjectManager",
 reply = manager.call("GetManagedObjects")
 if reply.type() != QDBusMessage.MessageType.ReplyMessage: exit(-1)
 ble_obj_paths = reply.arguments()[0]
-pprint(ble_obj_paths)
-for obj_path in ble_obj_paths:
-    if not "hci0" in obj_path: continue
-    pprint(obj_path)
+connected_bl_devs = []
+# pprint(ble_obj_paths)
+for obj_path, ifaces in ble_obj_paths.items():
+    if "hci0" not in obj_path or BluezDevice1Keys.interface not in ifaces.keys(): continue
+    conn_dev_info = ifaces[BluezDevice1Keys.interface]
+    bl_dev = Device1ServiceObject(adapter=conn_dev_info.get(BluezDevice1Keys.adapter), address=conn_dev_info.get(BluezDevice1Keys.address), 
+                                  address_type=conn_dev_info.get(BluezDevice1Keys.address_type), alias=conn_dev_info.get(BluezDevice1Keys.alias), 
+                                  blocked=conn_dev_info.get(BluezDevice1Keys.blocked),obj_class=conn_dev_info.get(BluezDevice1Keys.obj_class), 
+                                  connected=conn_dev_info.get(BluezDevice1Keys.connected), icon=conn_dev_info.get(BluezDevice1Keys.icon),
+                                  legacy_pairing=conn_dev_info.get(BluezDevice1Keys.legacy_pairing), modalias=conn_dev_info.get(BluezDevice1Keys.modalias),
+                                  name=conn_dev_info.get(BluezDevice1Keys.name), paired=conn_dev_info.get(BluezDevice1Keys.paired), 
+                                  services_resolved=conn_dev_info.get(BluezDevice1Keys.services_resolved), trusted=conn_dev_info.get(BluezDevice1Keys.trusted),
+                                  uuids=conn_dev_info.get(BluezDevice1Keys.uuids)
+                                  )
+    if bl_dev.trusted: connected_bl_devs.append(bl_dev)
+    
+pprint(connected_bl_devs)
 
 for conn_dev in dev.connectedDevices():
-    print(conn_dev.toString())
+    # print(conn_dev.toString())
+    ...
